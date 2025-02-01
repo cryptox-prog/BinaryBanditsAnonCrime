8110499b292392bca6b940eb4c84dbf6ee0d167f
Pranav Mudgil <pranavmudgi333@gmail.com>
2025-02-02 02:39:45 +0530

    Added CORS whitelist and Options

---
+++ config/allowedOrigins.js
@@ -0,0 +1,2 @@
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:3500', 'http://localhost:4500', 'http://localhost:5500', 'http://127.0.0.1:5500'];
+module.exports = allowedOrigins;
